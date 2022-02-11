194261019e329d88e892a071f0c387c9c355cb37
Zafer AYAN <ozcanzaferayan@gmail.com>
2022-02-11 10:53:09 +0300

    Initial Commit

---
+++ TestFacebookSDK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestFacebookSDK
+//
+//  Created by Zafer AYAN on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
